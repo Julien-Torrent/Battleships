aabac745dd774c939680beebdf75c1ccb8ff1da3
Julien Torrent <julien.torrent@outlook.com>
2018-10-15 19:07:20 +0200

    Add Ships

---
+++ Classes/Ships/Ship.cs
@@ -0,0 +1,19 @@
+﻿using Battleships.Classes.Cases;
+using System.Collections.Generic;
+
+namespace Battleships.Classes.Ships
+{
+    public class Ship
+    {
+        public bool IsVertical { get; private set; } = false;
+        public bool IsAlive { get; private set; } = true;
+        public string Name { get; private set; }
+
+        public List<ShipCase> Cases = new List<ShipCase>(5);
+
+        public Ship(Ships size)
+        {
+            Name = size.ToString();
+        }
+    }
+}
---
+++ Classes/Ships/Ships.cs
@@ -0,0 +1,11 @@
+﻿namespace Battleships.Classes.Ships
+{
+    public enum Ships
+    {
+        TorpedoBoat = 2,
+        Submarine = 3,
+        Destroyer = 3,
+        Cruiser = 4,
+        AircraftCarrier = 5
+    }
+}
